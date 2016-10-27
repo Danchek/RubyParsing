177cba62ab5a6cbcef5e136d10e4cf1aab568cdb
Denis Dotsenko <denisdotsenko7@gmail.com>
2016-10-27 15:37:38 +0300

    Parsing of audio podkast v1.0

--- app/controllers/parsings_controller.rb
+++ app/controllers/parsings_controller.rb
@@ -3,11 +3,8 @@ require 'nokogiri'
 require 'open-uri'
 # Class for parsing rss
 class ParsingsController < ApplicationController
-  # GET /parsings
-  # GET /parsings.json
   def index
-    # @parsings = Parsing.all
-   @parsings = []
+    @parsings = []
     page = Nokogiri::HTML(open('http://ai.libsyn.com/rss'))
     (0..2).each do |i|
       @parsings << page.css('enclosure')[i]['url']
